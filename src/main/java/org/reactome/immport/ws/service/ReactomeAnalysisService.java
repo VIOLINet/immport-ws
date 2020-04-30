3c38f69526ba24d26d72c9c19d9baf47b1b28c1f
Tim Brunson <tjbrunson@gmail.com>
2020-04-30 08:50:41 -0700

    added HTTP call to reactome analysis service

--- src/main/java/org/reactome/immport/ws/service/ReactomeAnalysisService.java
+++ src/main/java/org/reactome/immport/ws/service/ReactomeAnalysisService.java
@@ -1,7 +1,18 @@
 package org.reactome.immport.ws.service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.Collection;
 
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.RequestEntity;
+import org.apache.commons.httpclient.methods.StringRequestEntity;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -9,6 +20,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class ReactomeAnalysisService {
     private final Logger logger = Logger.getLogger(ReactomeAnalysisService.class);
+    
+    private final String HTTP_POST = "Post";
     
     @Autowired
     private ReactomeAnalysisConfig config;
@@ -22,7 +35,16 @@ public class ReactomeAnalysisService {
     }
     
     public String doPathwayEnrichmentAnalysis(Collection<String> collection) {
+    	String analysisText = "";
+    	try {
+    		analysisText = callHttp(config.getReactomeAnalysisURL(), HTTP_POST, collection.toString());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
         System.out.println("Reactome URL: " + config.getReactomeAnalysisURL());
+        System.out.println(analysisText);
         return "{\n" + 
                 "  \"summary\": {\n" + 
                 "    \"token\": \"MjAyMDA0MjkwMzUyMDBfNDU3OQ%3D%3D\",\n" + 
@@ -34,5 +56,40 @@ public class ReactomeAnalysisService {
                 "    \"includeDisease\": true\n" + 
                 "  }}";
     }
-
+    
+    private String callHttp(String url, String type, String query) throws IOException {
+    	PostMethod method = null;
+    	HttpClient client = null;
+    	
+    	method = new PostMethod(url);
+    	method.setRequestEntity(new StringRequestEntity(query, "text/plain", "UTF-8"));
+    	method.setRequestHeader("Accept", "application/json");
+    	
+    	client = new HttpClient();
+    	
+    	int responseCode = client.executeMethod(method);
+    	if(responseCode == HttpStatus.SC_OK) {
+    		InputStream is = method.getResponseBodyAsStream();
+    		return readMethodReturn(is);
+    	}
+    	else return "";
+    	
+    }
+    protected String readMethodReturn(InputStream is) throws IOException {
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader reader = new BufferedReader(isr);
+        StringBuilder builder = new StringBuilder();
+        String line = null;
+        while ((line = reader.readLine()) != null)
+            builder.append(line).append("\n");
+        reader.close();
+        isr.close();
+        is.close();
+        // Remove the last new line
+        String rtn = builder.toString();
+        // Just in case an empty string is returned
+        if (rtn.length() == 0)
+            return rtn;
+        return rtn.substring(0, rtn.length() - 1);
+    }
 }
